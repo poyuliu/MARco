cea8b8a0c6dedaac9ffda23c9361aa6bb1406375
poyuliu <cryptolaemus.montrouzieri@gmail.com>
2020-12-18 00:19:48 +0800

    update codes

---
+++ R/FN_KO2PathwayModule.R
@@ -0,0 +1,256 @@
+list.of.packages <- c("gage")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) {
+  if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+  BiocManager::install("gage")
+}
+rm(list = c("list.of.packages","new.packages"))
+
+#rm(list=ls())
+#picrust2 <- read.delim("KEGG/pred_metagenome_unstrat_descrip.tsv",row.names = 1)
+#picrust2 <- picrust2[,-1]
+#colnames(picrust2) <- gsub("Ke","Exp.",colnames(picrust2))
+#saveRDS(picrust2,file = "example_PICRUSt2.RDS")
+#gs.table <- KO2path(predtable = picrust2,"module")
+#gs.table <- KO2path(predtable = picrust2,"pathway")
+#lv2.table <- path.lvs(gs.table,2)
+
+# Example file: K. eiffingeri tadpole gut microbiome, PICRUSt2 metagenome prediction using KEGG database
+#picrust2 <- readRDS("example_PICRUSt2.RDS")
+#head(picrust2)
+
+# categorized KEGG orthology (KO) IDs [abundance table] to pathways or modules/ Genesets: all pathway, metabolic pathway, modules/ options: convert to proportional table 
+KO2path <- function(predtable,GeneSet="pathway",prop=FALSE){
+  if(GeneSet=="pathway"){
+    genesets <- readRDS(url("http://www.lifescipy.net/RcodeDB/KEGG/AA_KEGG_GENESETS.RDS"))
+  } else if(GeneSet=="metabolic"){
+    genesets <- readRDS(url("http://www.lifescipy.net/RcodeDB/KEGG/AA_KEGG_METABOLIC.RDS")) 
+  } else if(GeneSet=="module"){
+    genesets <- readRDS(url("http://www.lifescipy.net/RcodeDB/KEGG/AA_KEGG_MODULES.RDS"))
+    genesets <- sapply(genesets,"[[",1)
+    genesets <- sapply(genesets,as.character)
+  }
+
+  gs.table <- data.frame(matrix(NA,nrow = length(genesets),ncol = ncol(predtable)))
+  colnames(gs.table) <- colnames(predtable)
+  rownames(gs.table) <- names(genesets)
+  for(i in 1:length(genesets)){
+    m.idx <- match(genesets[[i]], rownames(predtable))
+    m.idx <- m.idx[!is.na(m.idx)]
+    if(length(m.idx)>1){
+      gs.table[i,] <- colSums(predtable[m.idx,])
+    } else if(length(m.idx)==1){
+      gs.table[i,] <- predtable[m.idx,]
+    } else gs.table[i,] <- 0
+  }
+  
+  gs.table <- gs.table[rowSums(gs.table) != 0,]
+  gs.table.p <- as.data.frame(prop.table(as.matrix(gs.table),margin = 2))
+  
+  if(prop==FALSE){
+    return(gs.table)
+  } else if(prop==TRUE){
+    return(gs.table.p)
+  }
+  
+}
+
+# sort pathway/module categoized table with functional levels
+path.lvs <- function(gs.table,aggregate.lv=NULL){
+  aggregate2dff <- function(data,groups,FUN){
+    agg.data <- aggregate(data,list(groups),FUN)
+    rownames(agg.data) <- agg.data[,1]
+    agg.data <- as.data.frame(agg.data[,-1])
+    return(agg.data)
+  }
+  if(substr(rownames(gs.table)[1],start = 1,stop = 1)=="M"){
+    lvs <- readRDS(url("http://www.lifescipy.net/RcodeDB/KEGG/AA_KEGG_M_LEVELS.RDS"))
+    lvs <- lvs[which(lvs$Level_4 %in% substr(rownames(gs.table),1,6)),]
+    gs.table <- gs.table[match(lvs$Level_4,substr(rownames(gs.table),1,6)),]
+    gs.table <- cbind(lvs,gs.table)
+    rownames(gs.table) <- lvs$Level_4
+    gs.table <- gs.table[,-4]
+    colnames(gs.table)[4] <- "Level_4"
+    if(is.null(aggregate.lv)){
+      gs.table <- gs.table
+    } else if(!is.null(aggregate.lv) & aggregate.lv <4){
+      gs.table <- gs.table.agg <- aggregate2df(gs.table[,-c(1:4)],gs.table[,aggregate.lv],sum)
+    }
+  } else if(substr(rownames(gs.table)[1],start = 1,stop = 1)=="k"){
+    lvs <- readRDS(url("http://www.lifescipy.net/RcodeDB/KEGG/AA_KEGG_LEVELS.RDS"))
+    lvs <- lvs[which(lvs$Level_3 %in% substr(rownames(gs.table),1,7)),]
+    gs.table <- gs.table[match(lvs$Level_3,substr(rownames(gs.table),1,7)),]
+    gs.table <- cbind(lvs,gs.table)
+    rownames(gs.table) <- lvs$Level_3
+    gs.table <- gs.table[,-3]
+    colnames(gs.table)[3] <- "Level_3"
+    if(is.null(aggregate.lv)){
+      gs.table <- gs.table
+    } else  if(!is.null(aggregate.lv) & aggregate.lv < 3){
+      gs.table <- gs.table.agg <- aggregate2dff(gs.table[,-c(1:3)],gs.table[,aggregate.lv],sum)
+    }
+  }
+    return(gs.table)
+}
+
+# FSEA: Functional Sets Enrichment Analysis
+# within test: compare to "expected value" within group/individual
+# between test: compare to reference group/individual (NOT COMPLETE)
+FSEA <- function(Fdata,Groups,FunctionalSets="pathway",SortLv=2,Test="within",alpha=0.05,plot=TRUE,barcol=NULL,legend=TRUE){
+  aggregate2dfx <- function(data,groups,FUN){
+    agg.data <- aggregate(data,list(groups),FUN)
+    rownames(agg.data) <- agg.data[,1]
+    agg.data <- as.data.frame(t(agg.data[,-1]))
+    return(agg.data)
+  }
+  
+  if(FunctionalSets=="pathway"){
+    kegg.gs <- readRDS(url("http://www.lifescipy.net/RcodeDB/KEGG/AA_KEGG_GENESETS.RDS"))
+    Levels.KEGG <- readRDS(url("http://www.lifescipy.net/RcodeDB/KEGG/AA_KEGG_LEVELS.RDS"))
+  } else if(FunctionalSets=="metabolic"){
+    kegg.gs <- readRDS(url("http://www.lifescipy.net/RcodeDB/KEGG/AA_KEGG_METABOLIC.RDS"))
+    Levels.KEGG <- readRDS(url("http://www.lifescipy.net/RcodeDB/KEGG/AA_KEGG_LEVELS.RDS"))
+  } else if(FunctionalSets=="module"){
+    kegg.gs <- readRDS(url("http://www.lifescipy.net/RcodeDB/KEGG/AA_KEGG_MODULES.RDS"))
+    kegg.gs <- sapply(kegg.gs,"[[",1)
+    kegg.gs <- sapply(kegg.gs,as.character)
+    Levels.KEGG <- readRDS(url("http://www.lifescipy.net/RcodeDB/KEGG/AA_KEGG_M_LEVELS.RDS"))
+  }
+  if(Test=="within"){
+    qval <- data.frame(matrix(nrow=length(kegg.gs),ncol=ncol(Fdata)),row.names = names(kegg.gs))
+    colnames(qval) <- colnames(Fdata)
+    for(i in 1:ncol(Fdata)){
+      PropData <- prop.table(as.matrix(Fdata)[,i])
+      gagetest <- gage::gage(PropData,gset=kegg.gs,rank.test = T,saaTest = gage::gs.zTest)
+      gagetest <- as.data.frame(gagetest[[1]])
+      gagetest <- gagetest[order(rownames(gagetest)),]
+      qval[,i] <- gagetest$q.val
+    }
+    EScore <- -log10(qval)
+    EScore <- EScore[!is.na(rowSums(EScore)),]
+    EScore.mean <- aggregate2dfx(t(EScore),Groups,mean)
+    EScore.sem <- aggregate2dfx(t(EScore),Groups,sd)/sqrt(as.vector(table(Groups)))
+    EScore.idx <- EScore.mean
+    EScore.idx[EScore.mean < (-log10(alpha))] <- 0
+    EScore.idx[EScore.mean > (-log10(alpha))] <- 1
+    EScore.mean <- EScore.mean[rowSums(EScore.idx)!=0,]
+    EScore.sem <- EScore.sem[rowSums(EScore.idx)!=0,]
+    EScore.mean.order <- EScore.mean[order(rowSums(EScore.mean)),]
+    EScore.sem.order <- EScore.sem[order(rowSums(EScore.mean)),]
+    if(FunctionalSets=="module"){
+      gagetest.Lv <- as.character(Levels.KEGG[,SortLv])[match(substr(rownames(EScore.mean.order),1,6),Levels.KEGG$Level_4)]
+    } else 
+      gagetest.Lv <- as.character(Levels.KEGG[,SortLv])[match(substr(rownames(EScore.mean.order),1,6),Levels.KEGG$Level_3)]
+    
+    EScore.mean.order <- EScore.mean.order[order(gagetest.Lv,decreasing = T),]
+    EScore.sem.order <- EScore.sem.order[order(gagetest.Lv,decreasing = T),]
+    EScore.order <- list(EScore.mean=EScore.mean.order,EScore.sem=EScore.sem.order)
+    
+    if(plot==TRUE){
+      par(mar=c(5.1,20.1,1.1,2.1),cex.lab=0.7,cex.axis=0.7)
+      bars <- barplot(t(EScore.mean.order),beside = T,horiz = T,
+                      las=1,xlab="Enrichment score",border = 0,cex.names = 0.7,
+                      col = barcol)
+      errobar <- as.data.frame(cbind(xaxis=as.vector(bars),
+                                     upper=as.vector(t(EScore.mean.order))+as.vector(t(EScore.sem.order)),
+                                     lower=as.vector(t(EScore.mean.order))-as.vector(t(EScore.sem.order))))
+      segments(x0 = errobar$upper,y0 = errobar$xaxis,x1 = errobar$lower,y1 = errobar$xaxis)
+      abline(v=-log10(alpha),lty=3)
+      if(legend==TRUE){
+        if(!is.null(barcol)){
+          legend("bottomright",legend = levels(as.factor(Groups)),bty="n",fill=barcol,cex = 0.7)
+        } else legend("bottomright",legend = levels(as.factor(Groups)),bty="n",
+                      fill=gray.colors(length(unique(as.factor(Groups)))),cex = 0.7)
+      }
+    }
+    
+    return(EScore.order)
+  } else if(Test=="between"){
+    PropData <- prop.table(as.matrix(Fdata),margin = 2)
+    gageX <- list()
+    cbm <- cbind(combn(levels(Groups),2),combn(levels(Groups),2)[2:1,])
+    for(k in 1:ncol(cbm)){
+      gageX[[k]] <- gage(PropData,gset=kegg.gs,ref=which(Groups==cbm[2,k]),samp=which(Groups==cbm[1,k]),rank.test = F,
+                         saaTest = gs.tTest,compare = "unpaired")
+      gageX[[k]] <- as.data.frame(gageX[[k]][[1]])
+      gageX[[k]] <- gageX[[k]][order(rownames(gageX[[k]])),]
+    }
+    
+    qvals <- data.frame(do.call(cbind,lapply(gageX,"[","q.val")))
+    colnames(qvals) <- paste0("r:",t(cbm)[,1],"-","s:",t(cbm)[,2])
+    qvals[is.na(qvals)] <- 1
+    qvals.sig <- qvals[which(rowSums(qvals<0.05)>=1),]
+    Escore.sig <- -log10(qvals.sig)
+    
+    ESx <- list()
+    for(m in 1:length(levels(Groups))){
+      ESx[[m]] <- Escore.sig[,grep(paste0("r:",levels(Groups)[m],"-"),names(Escore.sig))]
+      ESx[[m]] <- ESx[[m]][order(rowSums(ESx[[m]])),]
+    }
+    
+    if(plot==TRUE){
+      par(mar=c(4.1,18.1,1.1,2.1),cex.lab=0.7,cex.axis=0.7)
+      for(m in 1:length(levels(Groups))){
+        barplot(t(ESx[[m]]),beside = T,horiz = T,las=1,cex.name=0.7,xlim=c(0,12),col = barcol,border = F)
+        abline(v=-log10(0.05),lty=3)
+        legend("bottomright",legend = colnames(ESx[[m]]),
+               bty="n",fill=barcol,cex = 0.7)
+      }
+    }
+    
+    return(ESx)
+  }
+}
+
+
+# KO to metabolome
+KO2metabolite <- function(data.p){
+  kegg.rx <- readRDS(url("http://www.lifescipy.net/RcodeDB/KEGG/KO-RN-CPD.db.RDS"))
+  kegg.rx <- do.call(rbind,kegg.rx)
+  
+  cpd.w <- list()
+  for(i in 1:nrow(data.p)){
+    rx.i <- kegg.rx[which(kegg.rx$KO %in% as.character(data.p[i,1])),]
+    if(nrow(rx.i)>1){
+      rx.i <- rx.i[!duplicated(paste(rx.i$rn,rx.i$KO)),]
+      rx.i[which(rx.i$type=="irreversible"),c("substrate.1","substrate.2","substrate.3","substrate.4","substrate.5")] <- NA
+      
+      cpd.i <- data.frame(CID=matrix(t(rx.i[,5:14]),ncol = 1),weight=NA)
+      cpd.i <- cpd.i[!is.na(cpd.i$CID),]
+      if(nrow(cpd.i)>1){
+        subject.i <- matrix(apply(!is.na(rx.i[,5:9]),2,as.numeric),ncol = 5)
+        product.i <- matrix(apply(!is.na(rx.i[,10:14]),2,as.numeric),ncol = 5)
+        
+        subject.w <- subject.i/rowSums(subject.i)
+        subject.w[is.nan(subject.w)] <- 0
+        subject.w <- subject.w / (as.numeric(rowSums(product.i)>0) + as.numeric(rowSums(subject.i)>0))
+        product.w <- product.i/rowSums(product.i)
+        product.w[is.nan(product.w)] <- 0
+        product.w <- product.w  / (as.numeric(rowSums(product.i)>0) + as.numeric(rowSums(subject.i)>0))
+        subject.w[subject.w==0] <- NA
+        product.w[product.w==0] <- NA
+        weight <- cbind(subject.w, product.w)
+        weight <- matrix(t(weight),ncol = 1)
+        cpd.i$weight <- weight[!is.na(weight),]
+        cpd.w[[i]] <- aggregate(cpd.i$weight,list(cpd.i$CID),sum)
+      }
+    } else cpd.w[[i]] <- data.frame("Group.1"=NA,"x"=NA)
+  }
+  names(cpd.w) <- data.p$KO
+  
+  cpd.x <- cpd.w
+  sample.names <- names(data.p)
+  for(i in 1:length(cpd.x)){
+    for(j in 2:length(sample.names)){
+      if(!is.null(cpd.x[[i]])){
+        cpd.x[[i]]$newcol <- cpd.x[[i]]$x * data.p[i,j]
+        names(cpd.x[[i]])[j+1] <- sample.names[j]
+      }
+    }
+  }
+  
+  cpd.x <- do.call(rbind,cpd.x)
+  cpd.x <- aggregate(cpd.x[,-1],list(cpd.x$Group.1),sum)
+  return(cpd.x)
+}
---
+++ R/FN_RandomForest.R
@@ -0,0 +1,177 @@
+#list.of.packages <- c("randomForest","plyr","rfUtilities","caret","ROCR","pROC","doMC")
+#new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+#if(length(new.packages)) install.packages(new.packages)
+
+#required packages
+require("randomForest")
+require("plyr") # for the "arrange" function
+require("rfUtilities") # to test model significance
+require("caret") # to get leave-one-out cross-validation accuracies and also contains the nearZeroVar function 
+require("ROCR")
+require("pROC")
+require("doMC")
+
+#Removing Rare Features
+remove_rare <- function( table , cutoff_pro ) {
+  row2keep <- c()
+  cutoff <- ceiling( cutoff_pro * ncol(table) )  
+  for ( i in 1:nrow(table) ) {
+    row_nonzero <- length( which( table[ i , ]  > 0 ) ) 
+    if ( row_nonzero > cutoff ) {
+      row2keep <- c( row2keep , i)
+    }
+  }
+  return( table [ row2keep , , drop=F ])
+}
+
+
+
+#Transforming Your Data
+otu_trans_scale <- function(otu_table_rare_removed){
+  otu_table_rare_removed_norm <- sweep(otu_table_rare_removed, 2, colSums(otu_table_rare_removed) , '/')*100
+  otu_table_scaled <- scale(otu_table_rare_removed_norm, center = TRUE, scale = TRUE)
+  otu_table_scaled <- as.data.frame(otu_table_scaled)
+  otu_table_scaled
+}
+
+
+#Running Model
+#classification or regression
+runRF <- function(otu_table_scaled,Y,metadata,model=c("classification","regression"),ntree=501,cores=4,plotALL=FALSE,plotTOP10=FALSE){
+  try(if(model!="classification" & model!="regression") stop("Must select an RF model type: 'classification' or 'regression'!"))
+  otu_table_scaled_data <- data.frame(t(otu_table_scaled))  
+  attach(metadata)
+  otu_table_scaled_data$meta <- Y
+  
+  registerDoMC(cores) #number of cores on the machine
+  RF_model <- foreach(y=seq(10), .combine=combine ) %dopar% {
+    set.seed(151)
+    RF_model_parallel <- randomForest(x=otu_table_scaled_data[,1:(ncol(otu_table_scaled_data)-1)],
+                                      y=otu_table_scaled_data[ , ncol(otu_table_scaled_data)],
+                                      ntree=ntree, importance=TRUE, proximities=TRUE)
+  }
+  
+  #Permutation Test
+  RF_model_sig <- rf.significance(x=RF_model, xdata=otu_table_scaled_data[,1:(ncol(otu_table_scaled_data)-1)],nperm=1000 , ntree=ntree )  
+  
+  #Accuracy Estimated by Cross-validation
+  fit_control <- trainControl( method = "LOOCV" )
+  RF_model_loocv <- train(otu_table_scaled_data[,1:(ncol(otu_table_scaled_data)-1)],
+                          y=otu_table_scaled_data[, ncol(otu_table_scaled_data)],
+                          method="rf", ntree=ntree , tuneGrid=data.frame( mtry=RF_model$mtry ), trControl=fit_control )
+  
+  #Identifying Important Features
+  model <- match.arg(model,)
+  if(model=="classification"){
+    RF_model_classify_imp <- as.data.frame( RF_model$importance )
+    RF_model_classify_imp$features <- rownames( RF_model_classify_imp )
+    RF_model_classify_imp_sorted <- arrange( RF_model_classify_imp  , desc(MeanDecreaseAccuracy)  )
+    if(plotALL==TRUE & plotTOP10==TRUE){
+      par(mfrow=c(1,2))
+      barplot(RF_model_classify_imp_sorted$MeanDecreaseAccuracy, ylab="Mean Decrease in Accuracy (Variable Importance)", main="RF Classification Variable Importance Distribution")
+      barplot(RF_model_classify_imp_sorted[1:10,"MeanDecreaseAccuracy"], names.arg=RF_model_classify_imp_sorted[1:10,"features"] , ylab="Mean Decrease in Accuracy (Variable Importance)", las=2, main="Classification RF")  
+    } else if(plotALL==TRUE & plotTOP10==FALSE){
+      barplot(RF_model_classify_imp_sorted$MeanDecreaseAccuracy, ylab="Mean Decrease in Accuracy (Variable Importance)", main="RF Classification Variable Importance Distribution")
+    } else if(plotALL==FALSE & plotTOP10==TRUE){
+      barplot(RF_model_classify_imp_sorted[1:10,"MeanDecreaseAccuracy"], names.arg=RF_model_classify_imp_sorted[1:10,"features"] , ylab="Mean Decrease in Accuracy (Variable Importance)", las=2, main="Classification RF")
+    }
+    return(list(RFmodel=RF_model, PermuTest=RF_model_sig, LOOCV=RF_model_loocv, SortFeature=RF_model_classify_imp_sorted))
+    
+  } else if(model=="regression"){
+    RF_model_regress_imp <- as.data.frame( RF_model$importance )
+    RF_model_regress_imp$features <- rownames( RF_model_regress_imp )
+    RF_model_regress_imp_sorted <- arrange( RF_model_regress_imp  , desc(`%IncMSE`)  )
+    if(plotALL==TRUE & plotTOP10==TRUE){
+      par(mfrow=c(1,2))
+      barplot(RF_model_regress_imp_sorted$`%IncMSE`, ylab="% Increase in Mean Squared Error (Variable Importance)", main="RF Regression Variable Importance Distribution")
+      barplot(RF_model_regress_imp_sorted[1:10,"%IncMSE"], names.arg=RF_model_regress_imp_sorted[1:10,"features"] , ylab="% Increase in Mean Squared Error (Variable Importance)", las=2, main="Regression RF")  
+    } else if(plotALL==TRUE & plotTOP10==FALSE){
+      barplot(RF_model_regress_imp_sorted$`%IncMSE`, ylab="% Increase in Mean Squared Error (Variable Importance)", main="RF Regression Variable Importance Distribution")
+    } else if(plotALL==FALSE & plotTOP10==TRUE){
+      barplot(RF_model_regress_imp_sorted[1:10,"%IncMSE"], names.arg=RF_model_regress_imp_sorted[1:10,"features"] , ylab="% Increase in Mean Squared Error (Variable Importance)", las=2, main="Regression RF")  
+    }
+    return(list(RFmodel=RF_model, PermuTest=RF_model_sig, LOOCV=RF_model_loocv, SortFeature=RF_model_regress_imp_sorted))
+  }
+  
+}
+
+
+
+RF2ROC <- function(RFmodel,plot=FALSE,col="#003366"){
+  RFmodel <- RFmodel$RFmodel
+  Importance <- importance(RFmodel,type = 2)
+  predictions=as.vector(RFmodel$votes[,2])
+  pred=prediction(predictions,as.numeric(RFmodel$y)-1)
+  perf_AUC=performance(pred,"auc") #Calculate the AUC value
+  AUC=perf_AUC@y.values[[1]]
+  perf_ROC=performance(pred,"tpr","fpr") #plot the actual ROC curve
+  ci95 <- ci.auc(RFmodel$y,RFmodel$votes[,2])
+  diagnosis <- table(RFmodel$predicted,RFmodel$y)
+  accuracy <- sum(diag(diagnosis))/sum(diagnosis)
+  cutff <- which.min(abs(perf_ROC@alpha.values[[1]] - accuracy)) #best cutoff point
+  sensitivity <- perf_ROC@y.values[[1]][cutff]
+  specificity <- 1-perf_ROC@x.values[[1]][cutff]
+  if(plot==TRUE){
+    par(las=1,lwd=1.5,mar=c(5.1,4.1,4.1,10.1))
+    plot(perf_ROC,type="n",ylab="Sensitivity",xlab="1-Specificity")
+    abline(a = 0,b = 1,lwd=1.5,lty=3,col="gray50")
+    lines(perf_ROC@x.values[[1]],perf_ROC@y.values[[1]],col=col)
+    points(1-specificity,sensitivity,pch=17,col=col)
+    legend(x=1.02,y = 1,legend = c(paste0("AUROC = ",round(AUC,2)),
+                                   paste0("95% CI ",round(ci95[1],2)," - ",round(ci95[3],2)),
+                                   paste0("Sensitivity = ",round(sensitivity,2)),
+                                   paste0("Specificity = ",round(specificity,2))),
+           xpd=T,bty="n",cex=0.8,text.col = col)
+  }
+  return(list(AUC=AUC,perf_ROC=perf_ROC,CI95=ci95,Diagnosis=diagnosis,Accuracy=accuracy,Sensitivity=sensitivity,Specificity=specificity))
+}
+
+# validation cohort cross validation
+VCCV <- function(RFmodel,newdata,newdataGroup,plot=FALSE,cvcol="#bf0808",RFROC,mdcol="#003366"){
+  RFmodel <- RFmodel$RFmodel
+  
+  mapID <- match(rownames(RFmodel$importance),rownames(newdata))
+  otu.validataion <- newdata[mapID,]
+  rownames(otu.validataion) <- rownames(RFmodel$importance)
+  otu.validataion[is.na(otu.validataion)] <- 0
+  
+  CV.validataion <- predict(RFmodel,newdata = t(otu.validataion))
+  CV.validataion.vote <- predict(RFmodel,newdata = t(otu.validataion),type = "vote")
+  
+  pred.CV=prediction(CV.validataion.vote[,2],as.numeric(newdataGroup)-1)
+  perf_AUC.CV=performance(pred.CV,"auc") #Calculate the AUC value
+  AUC.CV=perf_AUC.CV@y.values[[1]]
+  perf_ROC.CV=performance(pred.CV,"tpr","fpr") #plot the actual ROC curve
+  
+  ci95.CV <- ci.auc(newdataGroup,CV.validataion.vote[,2])
+  diagnosis.CV <- table(CV.validataion,newdataGroup)
+  accuracy.CV <- sum(diag(diagnosis.CV))/sum(diagnosis.CV)
+  cutff.CV <- which.min(abs(perf_ROC.CV@alpha.values[[1]] - accuracy.CV))
+  sensitivity.CV <- perf_ROC.CV@y.values[[1]][cutff.CV]
+  specificity.CV <- 1-perf_ROC.CV@x.values[[1]][cutff.CV]
+  
+  if(plot==TRUE){
+    par(las=1,lwd=1.5,mar=c(5.1,4.1,4.1,10.1))
+    plot(perf_ROC.CV,type="n",ylab="Sensitivity",xlab="1-Specificity")
+    abline(a = 0,b = 1,lwd=1.5,lty=3,col="gray50")
+    #model AUROC
+    lines(RFROC$perf_ROC@x.values[[1]],RFROC$perf_ROC@y.values[[1]],col=mdcol)
+    points(1-RFROC$Specificity,RFROC$Sensitivity,pch=17,col=mdcol)
+    legend(x=1.02,y = 1,legend = c(paste0("AUROC = ",round(RFROC$AUC,2)),
+                                   paste0("95% CI ",round(RFROC$CI95[1],2)," - ",round(RFROC$CI95[3],2)),
+                                   paste0("Sensitivity = ",round(RFROC$Sensitivity,2)),
+                                   paste0("Specificity = ",round(RFROC$Specificity,2))),
+           xpd=T,bty="n",cex=0.8,text.col = mdcol,title = "Model")
+    
+    #validation AUROC
+    lines(perf_ROC.CV@x.values[[1]],perf_ROC.CV@y.values[[1]],col=cvcol,lty=2)
+    points(1-specificity.CV,sensitivity.CV,pch=2,col=cvcol)
+    legend(x=1.02,y = 0.3,legend = c(paste0("AUROC = ",round(AUC.CV,2)),
+                                   paste0("95% CI ",round(ci95.CV[1],2)," - ",round(ci95.CV[3],2)),
+                                   paste0("Sensitivity = ",round(sensitivity.CV,2)),
+                                   paste0("Specificity = ",round(specificity.CV,2))),
+           xpd=T,bty="n",cex=0.8,text.col = cvcol,title = "Validation Dataset")
+  }
+  
+  return(list(AUC=AUC.CV,perf_ROC=perf_ROC.CV,CI95=ci95.CV,Diagnosis=diagnosis.CV,Accuracy=accuracy.CV,Sensitivity=sensitivity.CV,Specificity=specificity.CV))
+}
